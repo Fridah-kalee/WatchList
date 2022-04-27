13f2e38ab31cd95b229e3828cdb96b5210534eab
Fridah-kalee <fridahkalee@gmail.com>
2022-04-27 07:15:14 +0300

    Appending information

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,15 @@
 from flask import Flask
 from .config import DevConfig
-app = Flask(__name__)
+from flask_bootstrap import Bootstrap
+
+# Initializing application
+app = Flask(__name__,instance_relative_config = True)
+
+# Setting up configuration
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
+# initializing bootstrap
+bootstarp =Bootstrap(app)
+
 from app import views
